e9e31562446438199aef3d5eacb58bf5e5dbc38d
izvanzemaljci <sanja.milovic@stu.ssst.edu.ba>
2021-01-15 12:39:37 +0100

    added scoring

--- fbc.py
+++ fbc.py
@@ -46,15 +46,36 @@ def bird_animation():
     new_bird_rect = new_bird.get_rect(center=(50,bird_rect.centery))
     return new_bird, new_bird_rect
 
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(int(score)), True,(255,255,255))
+        score_rect = score_surface.get_rect(center=(144,50))
+        screen.blit(score_surface,score_rect)
+    if game_state == 'game_over':
+        score_surface = game_font.render(f'Score: {int(score)}', True,(255,255,255))
+        score_rect = score_surface.get_rect(center=(144,50))
+        screen.blit(score_surface,score_rect)
+
+        high_score_surface = game_font.render(f'High score: {int(high_score)}', True,(255,255,255))
+        high_score_rect = high_score_surface.get_rect(center=(144,425))
+        screen.blit(high_score_surface,high_score_rect)
+
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
 
 pygame.init()
 screen = pygame.display.set_mode((288,512))
 clock = pygame.time.Clock()
+game_font = pygame.font.Font('04B_19.ttf',20)
 
 #Game Variables
 gravity = 0.35
 bird_movement = 0
 game_active = True
+score = 0
+high_score = 0
 
 bg_surface = pygame.image.load('assets/background-day.png').convert()
 floor_surface = pygame.image.load('assets/base.png').convert()
@@ -76,6 +97,9 @@ SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
 pipe_height = [200,300,400]
 
+game_over_surface = pygame.image.load('assets/message.png').convert_alpha()
+game_over_rect = game_over_surface.get_rect(center = (144,256))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -84,12 +108,13 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
-                bird_movement -= 10
+                bird_movement -= 8
             if event.key == pygame.K_SPACE and game_active == False:
                 game_active = True
                 pipe_list.clear()
                 bird_rect.center = (50,256)  
                 bird_movement = 0 
+                score = 0
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
         if event.type == BIRDFLAP:
@@ -112,6 +137,13 @@ while True:
         #Pipes
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
+
+        score += 0.01
+        score_display('main_game')
+    else:
+        screen.blit(game_over_surface,game_over_rect)
+        high_score = update_score(score, high_score)
+        score_display('game_over')
 
     #Floor
     floor_x_pos -= 1
